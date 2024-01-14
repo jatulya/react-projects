6d42060799439a9a8f707a39996a5600dd7477aa
J-Atulya <atulyajayakrishnan@gmail.com>
2024-01-14 17:17:13 +0530

    Components

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav><h1>WORDLE CODDLE</h1></nav>
+      <Board/>
+      <Keyboard/>
     </div>
   );
 }
---
+++ wordle/src/Components/Board.js
@@ -0,0 +1,13 @@
+// type rfce, then press enter key to get basic code
+
+import React from 'react' 
+
+function Board() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Board
