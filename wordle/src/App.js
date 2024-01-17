4a7f9bea4e9654e21c278870a2c2f2e0df2ed139
J-Atulya <atulyajayakrishnan@gmail.com>
2024-01-17 13:30:05 +0530

    boardDefault copied to App

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -2,13 +2,22 @@ import { createContext, useState } from 'react';
 import './App.css';
 import Board from './Components/Board';
 import Keyboard from './Components/Keyboard';
-import { boardDefault } from './Words'
 
 export const AppContext = createContext()
 //context api is used to pass states used in parent component to all 
 //the mentioned under Appcontext.provider. the values passed is given in the values
 
 function App() {
+  
+  const boardDefault = [
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""],
+  ]
+  
   const [board, setBoard] = useState(boardDefault)
 
   return (
