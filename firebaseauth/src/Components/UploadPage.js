e619dfb00c3fbd13da01d2f22b32245d5bb22fd6
J-Atulya <atulyajayakrishnan@gmail.com>
2024-04-28 15:22:47 +0530

    Both back to front

--- firebaseauth/src/Components/UploadPage.js
+++ firebaseauth/src/Components/UploadPage.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {auth} from '../firebase-config'
 import {signOut} from 'firebase/auth'
 import { useNavigate } from 'react-router-dom';
 
 const UploadPage = () => {
+
   let navigate = useNavigate();
     const logout = async() => {
         await signOut(auth);  
         navigate('/')
       };
-  // Assuming user data is passed as props
   const currUser = localStorage.getItem('currentUser')
+  const id = localStorage.getItem('userId')
+
+  const [test, setTest] = useState([])
+  const num = 12
+  //backend api
+  useEffect(()=>{
+    fetch(`/testing/${num}`).then(
+      res => res.json()
+    ).then(
+      data => {
+        setTest(data.sum)
+        console.log(data.sum)
+      })
+  },[num])
+
   return (
     <div>
       <h2>Upload Page</h2>
       <p>Welcome, {currUser}!</p>
       <button onClick={logout}>Sign out</button>
+      <div>This is from the backend {test}</div>
     </div>
   );
 };
